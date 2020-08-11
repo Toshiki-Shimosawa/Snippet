b931893c990ead7f2beccc421ee6358f3a40ef06
ProjectEuropa <m2delta29@gmail.com>
2020-08-11 20:49:48 +0900

    fix: #1

--- database/factories/SnippetFactory.php
+++ database/factories/SnippetFactory.php
@@ -1,9 +1,10 @@
 <?php
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\Snippet;
 use Faker\Generator as Faker;
 
-$factory->define(Model::class, function (Faker $faker) {
+$factory->define(Snippet::class, function (Faker $faker) {
     return [
         'title'=>$faker->word,
         'description'=>$faker->text,
